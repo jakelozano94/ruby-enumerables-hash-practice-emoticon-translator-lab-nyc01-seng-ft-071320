6df314ae462e157f90a3e47e4a22eec69732ff61
congenial-course-2234 <jakelozano94@gmail.com>
2020-07-09 19:24:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,7 +30,7 @@ def get_english_meaning(file_path, nip)
   emoticons.each do |key, value|
     value.each do |key2, value2|
     if nip == value2[0]
-      answer = key
+      answer == key
     end
     end
   end
