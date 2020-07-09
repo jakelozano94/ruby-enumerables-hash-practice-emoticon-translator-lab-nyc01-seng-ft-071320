fbef28b047e5aba5b5e5fc19859534a0de91ed3d
congenial-course-2234 <jakelozano94@gmail.com>
2020-07-09 19:02:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,28 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(yml)
+  emo = YAML.load_file(yml)
+  emo.each_with_object({}) do |(key, value), final_hash|
+  final_hash[key] = {:english => value[0], :japanese => value[1]}
+end
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(yml, usa)
+  load_library(yml)
+  final_hash.each do |key, value|
+    if usa ==value[1]
+      return value[0][0]
+    end
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(yml, nip)
+  load_library(yml)
+  final_hash.each do |key, value|
+    if nip == value[0]
+      return value [1][0]
+    end
+  end
 end
