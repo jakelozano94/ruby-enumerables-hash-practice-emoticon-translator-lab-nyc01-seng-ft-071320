48b5f5d7df1b30e3cdd75d0eb97f91cfafc701db
congenial-course-2234 <jakelozano94@gmail.com>
2020-07-09 19:18:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def get_japanese_emoticon(file_path, usa)
   emoticons.each do |key, value|
     value.each do |key2, value2|
     if usa ==value2[1]
-      return key[0]
+      return value[0]
     end
     end
   end
